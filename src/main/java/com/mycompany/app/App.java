5e78660a67504e0ebba92f283ce9323d847a5cef
kirbyydoge <aqwoguz@gmail.com>
2020-02-06 22:21:44 +0300

    Added untracked files

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,15 @@
+package com.mycompany.app;
+
+public class App {
+
+	public static boolean meaningful(Integer[] arr1, Integer[] arr2, int val1, int val2) {
+		if(arr1 == null || arr2 == null)
+			return false;
+		
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
